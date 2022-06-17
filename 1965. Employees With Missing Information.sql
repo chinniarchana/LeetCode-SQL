059a8ad035e97783d1c22785f75fdc3898cd88fa
Archana M <70010985+chinniarchana@users.noreply.github.com>
2022-06-17 22:31:16 +0530

    Add files via upload

---
+++ 1965. Employees With Missing Information.sql
@@ -0,0 +1,85 @@
+/*
+1965. Employees With Missing Information
+Table: Employees
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the name of the employee whose ID is employee_id.
+ 
+Table: Salaries
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the salary of the employee whose ID is employee_id.
+ 
+
+Write an SQL query to report the IDs of all the employees with missing information.
+The information of an employee is missing if:
+
+The employee's name is missing, or
+The employee's salary is missing.
+Return the result table ordered by employee_id in ascending order.
+
+Example 1:
+
+Input: 
+Employees table:
++-------------+----------+
+| employee_id | name     |
++-------------+----------+
+| 2           | Crew     |
+| 4           | Haven    |
+| 5           | Kristian |
++-------------+----------+
+Salaries table:
++-------------+--------+
+| employee_id | salary |
++-------------+--------+
+| 5           | 76071  |
+| 1           | 22517  |
+| 4           | 63539  |
++-------------+--------+
+Output: 
++-------------+
+| employee_id |
++-------------+
+| 1           |
+| 2           |
++-------------+
+Explanation: 
+Employees 1, 2, 4, and 5 are working at this company.
+The name of employee 1 is missing.
+The salary of employee 2 is missing.
+*/
+
+-- Solution for Recyclable and Low Fat Products
+
+SELECT employee_id 
+FROM Salaries 
+WHERE employee_id NOT IN (SELECT employee_id 
+                          FROM Employees)
+UNION
+
+SELECT employee_id 
+FROM Employees 
+WHERE employee_id NOT IN (SELECT employee_id 
+                          FROM Salaries)
+ORDER BY employee_id
+
+-- Alternative Solution
+
+SELECT T.employee_id 
+FROM (SELECT * FROM Employees 
+      UNION ALL
+      SELECT * FROM Salaries) T
+GROUP BY T.employee_id
+HAVING COUNT(T.employee_id) = 1
+ORDER BY T.employee_id ASC
