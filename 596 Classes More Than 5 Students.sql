8ab624157d6fcc7954560b1badad805d74f1f267
Archana M <70010985+chinniarchana@users.noreply.github.com>
2022-06-17 15:16:23 +0530

    Add files via upload

---
+++ 596 Classes More Than 5 Students.sql
@@ -0,0 +1,49 @@
+/*
+596. Classes More Than 5 Students
+Table: Courses
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key column for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+
+Write an SQL query to report all the classes that have at least five students. 
+Example 1:
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+Explanation: 
+- Math has 6 students, so we include it.
+- English has 1 student, so we do not include it.
+- Biology has 1 student, so we do not include it.
+- Computer has 1 student, so we do not include it.
+*/
+
+-- Solution for Classes More Than 5 Students
+
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(DISTINCT student) >= 5;
+
