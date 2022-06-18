df5c8e8ed81eb5cbae6c7a046a423697bb3b8929
Archana M <70010985+chinniarchana@users.noreply.github.com>
2022-06-18 20:32:10 +0530

    Add files via upload

---
+++ 197. Rising Temperature.sql
@@ -0,0 +1,54 @@
+/*
+197. Rising Temperature
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+*/
+
+-- Solution for Rising Temperature
+
+SELECT W1.id
+FROM Weather W1, Weather W2
+WHERE DATEDIFF(W1.recordDate, W2.recordDate) = 1
+AND W1.temperature > W2.temperature;
+
+
+SELECT W1.id 
+FROM Weather W1
+JOIN Weather W2
+ON W2.recordDate = SUBDATE(W1.recordDate, 1)
+AND W1.temperature > W2.temperature;
